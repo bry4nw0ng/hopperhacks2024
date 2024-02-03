4582b7b9a6c326da37da080ef89d07f4bdbd7173
Bryan <bryan@Bryans-MacBook-Pro.local>
2024-02-03 16:44:58 -0500

    ripple

--- circle.js
+++ circle.js
@@ -7,9 +7,14 @@ document.addEventListener("DOMContentLoaded", function() {
             circle.style.top = Math.random() * window.innerHeight + 'px';
             document.body.appendChild(circle);
 
-            // Add event listener to each circle
-            circle.addEventListener('mouseover', function() {
+            circle.addEventListener('mouseover', function(event) {
                 clearInterval(intervalId);
+
+                let redCircle = document.createElement('div');
+                redCircle.className = 'red-circle';
+                redCircle.style.left = event.clientX + 'px';
+                redCircle.style.top = event.clientY + 'px';
+                document.body.appendChild(redCircle);
             });
         }
     }, 150);
